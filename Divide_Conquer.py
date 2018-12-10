1d1f55441daf992cfa986d0669eb5a2f53d4094e
Nathaniel Tesfaye <ntesfaye@macalester.edu>
2018-12-10 15:05:07 -0600

    Included Divide and Conquer Algorithm

--- Divide_Conquer.py
+++ Divide_Conquer.py
@@ -1,1 +1,33 @@
-1
+import main
+
+def DivideAndConquerSingleSellProfit(arr):
+
+    if len(arr) <= 1:
+        return 0;
+
+
+    left  = arr[ : len(arr) / 2]
+    right = arr[len(arr) / 2 : ]
+
+    leftbest  = DivideAndConquerSingleSellProfit(left)
+    rightbest = DivideAndConquerSingleSellProfit(right)
+
+    crossbest = max(right) - min(left)
+
+    return max(leftbest, rightbest, crossbest)
+
+# arr = [1,2,3,4,5,6,7,8,9,15]
+#print(DivideAndConquerSingleSellProfit(arr))
+DJIClose = main.DJI_close_prices
+QQQClose = main.QQQ_close_prices
+SPYClose = main.SPY_close_prices
+for dp in DJIClose:
+    DivideAndConquerSingleSellProfit(dp)
+for qp in QQQClose:
+    DivideAndConquerSingleSellProfit(qp)
+for sp in SPYClose:
+    DivideAndConquerSingleSellProfit(sp)
+
+
+
+
