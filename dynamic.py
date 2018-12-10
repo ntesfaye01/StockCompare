7596e7b7a5b0bcd0a6e94beb825366300f1ca112
Jack <jfreier@macalester.edu>
2018-12-10 14:41:08 -0600

    added to main

--- dynamic.py
+++ dynamic.py
@@ -12,3 +12,19 @@ def dynamic_profit_max(prices):
             maximum_profit = (prices[i] - minimum_buy)
 
     return maximum_profit
+
+
+QQQ_maximum_profits = []
+for i in range(len(QQQ_close_prices)):
+    annual_max_profit = dynamic.dynamic_profit_max(QQQ_close_prices[i])
+    QQQ_maximum_profits.append(annual_max_profit)
+
+DJI_maximum_profits = []
+for i in range(len(DJI_close_prices)):
+    annual_max_profit = dynamic.dynamic_profit_max(DJI_close_prices[i])
+    DJI_maximum_profits.append(annual_max_profit)
+
+SPY_maximum_profits = []
+for i in range(len(SPY_close_prices)):
+    annual_max_profit = dynamic.dynamic_profit_max(SPY_close_prices[i])
+    SPY_maximum_profits.append(annual_max_profit)
--- main.py
+++ main.py
@@ -18,3 +18,18 @@ def pullData(csv_file_path):
 DJIclose = pullData('DJI.csv')
 SPYclose = pullData('SPY.csv')
 QQQclose = pullData('QQQ.csv')
+
+QQQ_maximum_profits = []
+for i in range(len(QQQ_close_prices)):
+    annual_max_profit = dynamic.dynamic_profit_max(QQQ_close_prices[i])
+    QQQ_maximum_profits.append(annual_max_profit)
+
+DJI_maximum_profits = []
+for i in range(len(DJI_close_prices)):
+    annual_max_profit = dynamic.dynamic_profit_max(DJI_close_prices[i])
+    DJI_maximum_profits.append(annual_max_profit)
+
+SPY_maximum_profits = []
+for i in range(len(SPY_close_prices)):
+    annual_max_profit = dynamic.dynamic_profit_max(SPY_close_prices[i])
+    SPY_maximum_profits.append(annual_max_profit)
